28a265133491f1eabb499b239b16ce0fd5e99198
CURSOTEC2024 <asaheld8@gmail.com>
2024-07-30 15:39:20 -0600

    project.v

--- src/project.v
+++ src/project.v
@@ -8,6 +8,7 @@
 module tt_um_example (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
+    
     input  wire [7:0] uio_in,   // IOs: Input path
     output wire [7:0] uio_out,  // IOs: Output path
     output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
@@ -17,9 +18,11 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  // assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  C_AND C1 (.a(ui_in[0]), .b(ui_in[1]), .c(uo_out[0]));
   assign uio_out = 0;
   assign uio_oe  = 0;
+  assign uio_in = 0;
 
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, clk, rst_n, 1'b0};
